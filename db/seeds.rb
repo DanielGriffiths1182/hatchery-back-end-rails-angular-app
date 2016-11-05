659fe4a94eecbe0925b369ddc78e4c0cc58abdf8
Daniel Griffiths <logiconictvtube@gmail.com>
2016-11-05 18:13:43 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+egg_count = [
+  [ 0, 0 ]
+]
+
+egg_count.each do |total, user_eggs|
+  Egg.create( total: total, user_eggs: user_eggs )
+end
