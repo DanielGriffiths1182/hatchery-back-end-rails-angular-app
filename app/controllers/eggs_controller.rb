474129d19f2660702bc11983d8126426131cb525
nsemmler <ndsunc@gmail.com>
2016-11-05 12:42:50 -0400

    refactored weather underground APIs again

--- app/controllers/eggs_controller.rb
+++ app/controllers/eggs_controller.rb
@@ -44,10 +44,16 @@ class EggsController < ApplicationController
   end
 
   def weather
-    conditions = get_conditions
-    sunrise = get_sunrise
-    sunset = get_sunset
-    temp = get_current_temp
+    forecast = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/forecast/q/27701.json')['forecast']['simpleforecast']['forecastday'][0]
+
+    conditions = forecast['conditions']
+
+    sun_times = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/astronomy/q/27701.json')['sun_phase']
+
+    sunrise = "#{sun_times['sunrise']['hour']}:#{sun_times['sunrise']['minute']}"
+    sunset = "#{sun_times['sunset']['hour']}:#{sun_times['sunset']['minute']}"
+
+    temp = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/conditions/q/27701.json')['current_observation']['temperature_string']
 
     @conditions = {
       'temperature' => temp,
@@ -60,32 +66,6 @@ class EggsController < ApplicationController
 
   def get_json(url)
     HTTParty.get(url).parsed_response
-  end
-
-  def get_conditions
-    forecast = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/forecast/q/27701.json')['forecast']['simpleforecast']['forecastday'][0]
-
-    conditions = forecast['conditions']
-  end
-
-  def get_sunrise
-    sun_times = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/astronomy/q/27701.json')['sun_phase']
-
-    sunrise_hour = sun_times['sunrise']['hour']
-    sunrise_minute = sun_times['sunrise']['minute']
-    sunrise = "#{sunrise_hour}:#{sunrise_minute}"
-  end
-
-  def get_sunset
-    sun_times = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/astronomy/q/27701.json')['sun_phase']
-
-    sunset_hour = sun_times['sunset']['hour']
-    sunset_minute = sun_times['sunset']['minute']
-    sunset = "#{sunset_hour}:#{sunset_minute}"
-  end
-
-  def get_current_temp
-    temp = get_json('http://api.wunderground.com/api/ea5d08a7d6566e2b/conditions/q/27701.json')['current_observation']['temperature_string']
   end
 
   private
